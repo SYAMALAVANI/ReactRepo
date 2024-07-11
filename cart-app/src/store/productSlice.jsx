3cd99bc9713093b46db998593111b08327529473
SYAMALAVANI <78420988+SYAMALAVANI@users.noreply.github.com>
2024-07-11 11:59:52 +0530

    Add files via upload

---
+++ cart-app/src/store/productSlice.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-refresh/only-export-components */
+import {  createAsyncThunk } from "@reduxjs/toolkit";
+
+import axios from "axios";
+
+const STATUSES = {
+  IDLE: "idle",
+  ERROR: "error",
+  LOADING: "loading..",
+};
+
+const productSlice = {
+  name: "product",
+
+  initialState: {
+    data: [],
+    status: STATUSES.IDLE,
+  },
+
+  extraReducers: (builder) => {
+    builder.addCase(fetchProducts.pending, (state, _action) => {
+      state.status = STATUSES.LOADING;
+    });
+    builder.addCase(fetchProducts.fulfilled, (state, action) => {
+      state.data = action.payload;
+      state.status = STATUSES.IDLE;
+    });
+    builder.addCase(fetchProducts.rejected, (state, _action) => {
+      state.status = STATUSES.ERROR;
+    });
+  },
+};
+
+// thunks
+
+export const fetchProducts = createAsyncThunk("products/fetch", async () => {
+  const res = await axios.get("https://fakestoreapi.com/products");
+  const data = res.data;
+  return data;
+});
+
+export default productSlice.reducer;
